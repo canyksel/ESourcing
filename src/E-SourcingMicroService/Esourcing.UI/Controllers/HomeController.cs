4463a76f53c7897121e7c4b021d4efc94c7cae94
Erdoğancan Yüksel <51403455+canyksel@users.noreply.github.com>
2021-11-22 22:45:25 +0300

    Jquery client-side validation added. Layaout edited.

--- src/E-SourcingMicroService/Esourcing.UI/Controllers/HomeController.cs
+++ src/E-SourcingMicroService/Esourcing.UI/Controllers/HomeController.cs
@@ -1,10 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ESourcing.UI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Esourcing.UI.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginModel)
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(AppUserViewModel signupModel)
         {
             return View();
         }
